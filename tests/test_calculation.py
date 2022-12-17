8a8f02e7b1fb47b0914c9d32065176b9949f272d
Chris Nyland <chris.nyland@gmail.com>
2022-12-17 22:08:43 +0000

    rename simple_calculation

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -4,28 +4,28 @@ from brass.bands import SlabbedBands, SteppedBands
 from brass.calculation import Calculation
 
 
-def test_simple_calculation_slabbed_bands_threshold_not_breached():
+def test_calculation_slabbed_bands_threshold_not_breached():
     taxable_amount = Decimal(100_000)
     bands = SlabbedBands(values=((100_000, 0), (200_000, 1)))
 
     assert Calculation(bands, taxable_amount).liability == Decimal("0")
 
 
-def test_simple_calculation_slabbed_bands_threshold_breached():
+def test_calculation_slabbed_bands_threshold_breached():
     taxable_amount = Decimal(150_000)
     bands = SlabbedBands(values=((100_000, 0), (200_000, 1)))
 
     assert Calculation(bands, taxable_amount).liability == Decimal("1500")
 
 
-def test_simple_calculation_stepped_bands_threshold_not_breached():
+def test_calculation_stepped_bands_threshold_not_breached():
     taxable_amount = Decimal(100_000)
     bands = SteppedBands(values=((100_000, 0), (200_000, 1)))
 
     assert Calculation(bands, taxable_amount).liability == Decimal("0")
 
 
-def test_simple_calculation_stepped_bands_threshold_breached():
+def test_calculation_stepped_bands_threshold_breached():
     taxable_amount = Decimal(150_000)
     bands = SteppedBands(values=((100_000, 0), (200_000, 1)))
 
